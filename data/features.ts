6a483f29c0370fb416f47d8a0c21c80b0381cfd9
Abhay Gupta <129143621+Abs6187@users.noreply.github.com>
2025-05-07 12:41:41 +0530

    Add files via upload

---
+++ data/features.ts
@@ -0,0 +1,46 @@
+import { FaVideo, FaDownload, FaCloudUploadAlt, FaUserAlt, FaThumbsUp ,FaHistory } from 'react-icons/fa';
+
+export const features = [
+  {
+    icon: FaVideo, // Video-related icon
+    name: "Stream High-Quality Videos",
+    description:
+      "Enjoy seamless streaming with top-quality resolution, including 4K, for a stunning experience.",
+    href: "#",
+  },
+  {
+    icon: FaDownload, // Download-related icon
+    name: "Download Your Favorites",
+    description:
+      "Download your favorite videos and access them offline anytime, anywhere.",
+    href: "#",
+  },
+  {
+    icon: FaCloudUploadAlt, // Upload-related icon
+    name: "Upload Content Easily",
+    description:
+      "Upload your own videos in various formats and share your creativity with the world.",
+    href: "#",
+  },
+  {
+    icon: FaUserAlt, // User profile icon
+    name: "Personalized Experience",
+    description:
+      "Create your profile, follow creators, and get tailored recommendations just for you.",
+    href: "#",
+  },
+  {
+    icon: FaThumbsUp, // Thumbs up (Like) icon
+    name: "Engage & Interact",
+    description:
+      "Like, comment, and share videos to engage with your favorite creators and the community.",
+    href: "#",
+  },
+  {
+    icon: FaHistory, // History icon
+    name: "Watch History & Recommendations",
+    description:
+      "Easily access your watch history and receive personalized recommendations based on your viewing habits.",
+    href: "#",
+  }
+];
