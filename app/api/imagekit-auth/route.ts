ce419551d8085e83fc8b8f32b8f94f109e91a47d
Abhay Gupta <129143621+Abs6187@users.noreply.github.com>
2025-05-08 01:30:25 +0530

    Update route.ts

--- app/api/imagekit-auth/route.ts
+++ app/api/imagekit-auth/route.ts
@@ -1,15 +1,15 @@
 import ImageKit from "imagekit"
 import { NextResponse } from "next/server";
-import { envConfig } from "@/lib/env";
 
 const imagekit = new ImageKit({
-  publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY || envConfig.NEXT_PUBLIC_PUBLIC_KEY,
-  privateKey: process.env.IMAGEKIT_PRIVATE_KEY || envConfig.IMAGEKIT_PRIVATE_KEY,
-  urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT || envConfig.NEXT_PUBLIC_URL_ENDPOINT,
+  publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+  urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT!,
 });
 
 export async function GET() {
     try {
+
         const authenticationParameters = imagekit.getAuthenticationParameters()
         return NextResponse.json(
             authenticationParameters
@@ -21,4 +21,5 @@ export async function GET() {
             {status: 500}
         )
     }
+  
 }
