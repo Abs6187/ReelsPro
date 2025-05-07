e9d7503d8f1a0afbd2c29f17da5bb7740335290e
Abhay Gupta <129143621+Abs6187@users.noreply.github.com>
2025-05-07 12:45:24 +0530

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Providers from "./components/Providers";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Reels Pro",
+  description: "Showcase your creativity! Upload, share, and explore amazing reels on Reels Pro.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        
+        <Providers>
+        
+         <main className=" w-full bg-black  ">
+          <Header />
+          {children}
+          <Footer />
+          </main>
+        </Providers>
+      </body>
+    </html>
+  );
+}
